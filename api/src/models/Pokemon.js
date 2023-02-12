203567d046f2b51176653e1249c6895780ec0624
Sil-paulli <sil.pauli.93@gmai.com>
2023-02-12 01:20:16 -0300

    db-listo

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,53 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique:true
     },
+    vida: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+   ataque: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    fuerza: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    defensa: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    velocidad: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    altura: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    peso: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    img: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    createdInDb: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
   });
 };
